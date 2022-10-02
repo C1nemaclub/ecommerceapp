d68fb686587d1b8c4373ef94b219e71aa0d467c8
Santiago <santiagov801@gmail.com>
2022-10-01 22:59:38 -0500

    sanity hookup/herobanner

---
+++ components/FooterBanner.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FooterBanner() {
+  return <div>FooterBanner</div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import { Product, HeroBanner, FooterBanner } from '../components';
+import { client } from '../lib/client';
 
-export default function index() {
-  return <div>index</div>;
+export default function index({ products, bannerData }) {
+  console.log(bannerData);
+
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className='products-container'>
+        {products?.map((product) => {
+          return product.name;
+        })}
+      </div>
+      <FooterBanner />
+    </>
+  );
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
